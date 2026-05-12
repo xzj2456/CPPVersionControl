25f9341b6463339df7acd46fe9e2faaf1570a9d2
xiong zhijun <2106454868@qq.com>
2026-05-12 11:30:56 +0800

    say bye

--- CPPVersionControl/main.cpp
+++ CPPVersionControl/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+
+void Sayhello()
+{
+	cout << "hello" << endl;
+}
+
 int main()
 {
+	Sayhello();
 	cout << "main function feature" << endl;
 	cout << "main function main" << endl;
 
